fee388a0f445f40770359054a0ed8823a3fa643b
Zachary Davison <zac.developer@gmail.com>
2026-04-15 01:24:28 +0200

    feat: add ZacLayout wrapper component

---
+++ src/components/ZacLayout.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template v-for="(_, name) in $slots" #[name]="slotData">
+      <slot :name="name" v-bind="slotData || {}" />
+    </template>
+  </Layout>
+</template>
